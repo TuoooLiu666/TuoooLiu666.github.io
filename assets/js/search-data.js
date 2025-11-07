967539e8bf7c9562b6494340d706417b7ae8d2a3
TuoooLiu666 <65417695+TuoooLiu666@users.noreply.github.com>
2025-11-07 03:32:20 +0000

    Deploying to gh-pages from @ TuoooLiu666/TuoooLiu666.github.io@e5effe5e3f19d6bfda80ac50865ba264bd2e9b9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of cool projects.",
+          description: "A growing collection of research and ds projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
